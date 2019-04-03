b49e9d3ebab0dccb0b454ccd6002e4aa8533edd1
Matt <maekozlowski@gmail.com>
2019-04-03 16:19:23 -0600

    Created Random dog generator

---
+++ index.js
@@ -0,0 +1,28 @@
+function displayDogs(responseJson){
+    console.log(responseJson);
+    $('.results').append(`<img src="${responseJson.message}" class="dogImages">`);
+    $('.results').removeClass('hidden');
+}
+
+function getDogImages(userInput){
+    for ( let i = 0; i < userInput; i++) {
+        fetch('https://dog.ceo/api/breeds/image/random')
+        .then(response => response.json())
+        .then(responseJson => displayDogs(responseJson));
+    }
+    
+}
+
+function watchForm(){
+    $('form').submit(function(event){
+        event.preventDefault();
+       let userInput = $('#userInput').val();
+       console.log(userInput);
+        getDogImages(userInput);
+    })
+}
+
+$(function(){
+    console.log('Dog randomizer loaded! Waiting for input.');
+    watchForm();
+})
